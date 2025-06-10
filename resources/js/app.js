851d81389d902a4a21fb377a43d7992b876a6360
arlindbikliqi1 <125808167+arlindbikliqi1@users.noreply.github.com>
2025-06-10 16:44:11 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,4 @@
-import './bootstrap';
-import './echo.js'
-import './dashboard';
-import './swiper';
-import './flatpickr';
-import './cityswap';
-import './documentModal';
-import './successErrorMessage';
-import './camera';
-import './loadSpinner';
-import Chart from 'chart.js/auto';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import './qr-code';
 
-import Alpine from 'alpinejs';
-import Intersect from '@alpinejs/intersect';
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
